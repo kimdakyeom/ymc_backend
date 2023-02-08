0e3e88fd90a3019d819a7771113ab823f7917827
pangpyo <kkp0639@gmail.com>
2023-02-08 23:40:17 +0900

    feat : 팀, 구장 모델 생성 및 회원가입시 팀 선택하게 한다

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 from django.core.validators import MinValueValidator, MaxValueValidator
+from articles.models import Team
 
 
 class UserManager(BaseUserManager):
@@ -48,6 +49,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["nickname"]
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.nickname
